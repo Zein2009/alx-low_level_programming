5158f3eb1032627ca929be4a0961cef63761fc5f
Ali Zein <eng.elandy@gmail.com>
2023-07-14 19:22:16 -0700

    update

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int void(main)
+{
+	/* here we will reverse the counting of the alphabets */
+
+	char ch;
+
+	for (ch = 'z'; ch > 'a'; ch--)
+	{
+		putchar (ch);
+	}
+	putchar("\n");
+	return (0);
+}
