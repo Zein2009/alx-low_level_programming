82dd85575f5dfef8bed1f530377c040c235e92e3
Ali Zein <eng.elandy@gmail.com>
2023-07-15 20:42:24 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * the main function
+ * return is (0) always success
+ */
+
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(str);
-	return(0);
+	return (0);
 }
